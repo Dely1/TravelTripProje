e4f4cae07f5ec1a021559d74738bed602db44990
Dely1 <asus@ROG>
2023-07-14 23:48:41 +0300

    Proje dosyası ekle.

---
+++ TravelTripProje/Controllers/GirişYapController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TravelTripProje.Controllers
+{
+    public class GirişYapController : Controller
+    {
+        // GET: GirişYap
+     
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ TravelTripProje/Models/Sınıflar/AnaSayfa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelTripProje.Models.Sınıflar
+{
+    public class AnaSayfa
+    {
+         [Key]
+        public int Id { get; set; }
+        public string Baslık { get; set; }
+        public string Acıklama { get; set; }
+    }
+}
